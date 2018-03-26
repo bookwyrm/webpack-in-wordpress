3204a36906a79331ef2bdfba2cae28ab95089b73
Matt Vanderpol <matt@vanderpol.net>
2018-03-26 06:33:25 -0700

    Make webpack understand css import

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  entry: './src/js/index.js'
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'css-loader' ]
+      }
+    ]
+  }
 };
