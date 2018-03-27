569c276276532023ab77a3fa980f75561231156b
Matt Vanderpol <matt@vanderpol.net>
2018-03-27 06:00:28 -0700

    Extract CSS to separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   entry: {
     bundle: './src/js/index.js',
     style: './src/css/style.css'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [ 'css-loader' ]
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       }
     ]
   }
