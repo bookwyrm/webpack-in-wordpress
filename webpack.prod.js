75002a8989be9d920d4641cdc6a7ecbd4fb550e5
Matt Vanderpol <matt@vanderpol.net>
2018-04-02 05:54:34 -0700

    Initial production webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,55 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ExtraneousFileCleanupPlugin = require('webpack-extraneous-file-cleanup-plugin');
+const autoprefixer = require('autoprefixer')
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style-prod.css'
+    }),
+    new ExtraneousFileCleanupPlugin({
+      extensions: [ '.js' ],
+      minBytes: 8000 // Minimum bytes to keep
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader?sourceMap=true',
+          {
+            loader: 'postcss-loader', options: {
+              sourceMap: true,
+              plugins() {
+                return [
+                  autoprefixer({
+                    browsers: ['last 2 versions']
+                  })
+                ]
+              }
+            }
+          },
+          'sass-loader?sourceMap=true'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  }
+});
+
