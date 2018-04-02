0aca3197788526d4b34bf2fef956b85b70cd3dd5
Matt Vanderpol <matt@vanderpol.net>
2018-04-02 05:20:57 -0700

    Add cloneDeep sample usage

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 var cloneDeep = require('lodash.clonedeep');
+var a = {
+  b: [
+    'c',
+    {
+      d: [ 1, 2, 3 ]
+    }
+  ]
+};
+
+var aa = cloneDeep(a);
+console.log("a === aa? ", a === aa);
 console.log("index.js")
+
