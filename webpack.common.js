89075aa2b3ad325855e1fa2868b7fbb29521ab58
Matt Vanderpol <matt@vanderpol.net>
2018-04-02 05:57:03 -0700

    Use webpack clean plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,11 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 module.exports = {
   entry: {
     bundle: './src/js/index.js',
     style: './src/sass/style.scss'
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ]
 }
