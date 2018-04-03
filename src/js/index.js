ce29b624257b05e46a5b951d4c019d92acc68fab
Matt Vanderpol <matt@vanderpol.net>
2018-04-03 11:25:46 -0700

    Example jQuery in JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import { cube } from './math.js';
+import 'jquery';
+
 const arr = [1, 2, 3];
 const test = () => console.log(...arr);
+
+jQuery('h1').css('background', '#f0f0f0');
 console.log("index.js");
 console.log("5 cubed is equal to " + cube(5));
 test();
