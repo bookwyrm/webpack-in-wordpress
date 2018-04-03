b4e22a236a16c79d5755417e3bb20173c602d747
Matt Vanderpol <matt@vanderpol.net>
2018-04-03 08:56:22 -0700

    Process JS with babel for ES5 transpilation

--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,13 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   }
