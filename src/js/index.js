6293d6a677f6b8bf7ffb12dbb88ffe17d3bc8733
Matt Vanderpol <matt@vanderpol.net>
2018-04-03 08:56:07 -0700

    Add ES6 sample code

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import { cube } from './math.js';
+const arr = [1, 2, 3];
+const test = () => console.log(...arr);
 console.log("index.js");
 console.log("5 cubed is equal to " + cube(5));
+test();
 
